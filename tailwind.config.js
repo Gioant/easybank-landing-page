8498fe50b48231b48010e6ab031ede6fa63c99b9
Anthony <gioant1@hotmail.com>
2023-10-26 18:40:40 -0400

    added tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/js/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
