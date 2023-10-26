4e5cd70bc1545e8d866d3e4f26bdc3c7cb234a9b
Anthony <gioant1@hotmail.com>
2023-10-26 19:16:00 -0400

    added custom classes for font family & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./src/js/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontfamily: {
+        'public-sans': ["'Public Sans'", 'sans-serif'],
+      },
+
+      colors: {
+        //Primary
+        'Space-Cadet': '#2d314d',
+        'Lime-Green': '#31d35c',
+        'Aero': '#2bb7da',
+        //Neutral
+        'Cool-Gray': '##9698A6',
+        'Flash-White': '##f3f4f6',
+        'SeaSalt': '#fafafa',
+        'White': '#ffffff',
+      },
+
+    },
   },
   plugins: [],
 }
